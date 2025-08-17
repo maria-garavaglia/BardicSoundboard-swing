1e72c9dd7ce6ab5d27bd932e6d36b7213cc7efed
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-08-16 22:30:57 -0400

    Adds all main window controls, no functionality yet

--- src/main/java/com/dearmariarenie/bardicsoundboard/Application.java
+++ src/main/java/com/dearmariarenie/bardicsoundboard/Application.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * Bootstrap class for application. Shouldn't do much besides initialize Spring and show the main window.
+ */
 @SpringBootApplication
 public class Application
 {
     public static void main(String[] args)
     {
+        // Spring typically starts headless, need to explicitly turn that off so Swing can run
         new SpringApplicationBuilder(Application.class)
             .headless(false)
             .run(args)
--- src/main/java/com/dearmariarenie/bardicsoundboard/MainWindowView.java
+++ src/main/java/com/dearmariarenie/bardicsoundboard/MainWindowView.java
@@ -1,14 +1,28 @@
 package com.dearmariarenie.bardicsoundboard;
 
+import java.awt.Dimension;
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JSlider;
 import javax.swing.WindowConstants;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MainWindowView extends JFrame
 {
+    private final JButton addSpellButton = new JButton("Add Spell");
+    private final JButton editSpellButton = new JButton("Edit Spell");
+    private final JButton removeSpellButton = new JButton("Remove Spell");
+    private final JList<String> spellList = new JList<>();
+    private final JLabel nowPlayingDisplay = new JLabel();
+    private final JButton playButton = new JButton("Play");
+    private final JButton stopButton = new JButton("Stop");
+    private final JSlider volumeSlider = new JSlider();
+
     public MainWindowView()
     {
         createUi();
@@ -17,8 +31,9 @@ public class MainWindowView extends JFrame
     public void createUi()
     {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(640, 480));
 
-        var helloWorldLabel = new JLabel("Hello World!");
+        var nowPlayingLabel = new JLabel("Now Playing:");
 
         var layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -26,13 +41,47 @@ public class MainWindowView extends JFrame
         layout.setAutoCreateContainerGaps(true);
 
         layout.setHorizontalGroup(
-            layout.createSequentialGroup()
-            .addComponent(helloWorldLabel)
+            layout.createParallelGroup()
+            .addGroup(
+                layout.createSequentialGroup()
+                .addComponent(addSpellButton)
+                .addComponent(editSpellButton)
+                .addComponent(removeSpellButton)
+            )
+            .addComponent(spellList, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE)
+            .addGroup(
+                layout.createSequentialGroup()
+                .addComponent(nowPlayingLabel)
+                .addComponent(nowPlayingDisplay, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE)
+            )
+            .addGroup(
+                layout.createSequentialGroup()
+                .addComponent(playButton)
+                .addComponent(stopButton)
+                .addComponent(volumeSlider, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+            )
         );
 
         layout.setVerticalGroup(
             layout.createSequentialGroup()
-            .addComponent(helloWorldLabel)
+            .addGroup(
+                layout.createParallelGroup()
+                .addComponent(addSpellButton)
+                .addComponent(editSpellButton)
+                .addComponent(removeSpellButton)
+            )
+            .addComponent(spellList, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE)
+            .addGroup(
+                layout.createParallelGroup()
+                .addComponent(nowPlayingLabel)
+                .addComponent(nowPlayingDisplay)
+            )
+            .addGroup(
+                layout.createParallelGroup(Alignment.CENTER)
+                .addComponent(playButton)
+                .addComponent(stopButton)
+                .addComponent(volumeSlider)
+            )
         );
     }
 
