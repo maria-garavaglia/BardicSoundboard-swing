75e31916455317df5b7c0268eeb587586b3af9a5
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-08-18 12:34:18 -0400

    Adds save/load character functions

--- src/main/java/com/dearmariarenie/bardicsoundboard/models/CharacterModel.java
+++ src/main/java/com/dearmariarenie/bardicsoundboard/models/CharacterModel.java
@@ -1,15 +1,31 @@
 package com.dearmariarenie.bardicsoundboard.models;
 
 import com.dearmariarenie.bardicsoundboard.utils.Fmt;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CharacterModel
 {
-    private String name;
-    private final Map<String, Path> spells;
+    @JsonProperty("charName")
+    private String name = "";
+    @JsonProperty
+    private Map<String, String> spells = new HashMap<>();
 
-    public CharacterModel(String name, Map<String, Path> spells)
+    public CharacterModel()
+    {
+    }
+
+    public CharacterModel(String name)
+    {
+        this.name = name;
+    }
+
+    public CharacterModel(String name, Map<String, String> spells)
     {
         this.name = name;
         this.spells = spells;
@@ -20,12 +36,18 @@ public class CharacterModel
         return name;
     }
 
-    public void setName(String name)
+    public CharacterModel setName(String name)
     {
         this.name = name;
+        return this;
     }
 
-    public void addSpell(String spellName, Path audioFile)
+    public Map<String, String> getSpells()
+    {
+        return spells;
+    }
+
+    public CharacterModel addSpell(String spellName, String audioFilename)
     {
         if (spells.containsKey(spellName))
         {
@@ -35,29 +57,29 @@ public class CharacterModel
             );
         }
 
-        // TODO check if audio file exists? Not sure if that should be here or
-        //  elsewhere. Or maybe in multiple places for redundancy?
-
-        spells.put(spellName, audioFile);
+        spells.put(spellName, audioFilename);
+        return this;
     }
 
-    public void editSpell(
+    public CharacterModel editSpell(
         String oldSpellName,
         String newSpellName,
-        Path newAudioFile
+        String newAudioFilename
     ){
         // Just remove the old spell entry and add a new one, it's easier than
         // futzing around with renaming existing entries
         removeSpell(oldSpellName);
-        addSpell(newSpellName, newAudioFile);
+        addSpell(newSpellName, newAudioFilename);
+        return this;
     }
 
-    public void removeSpell(String spellName)
+    public CharacterModel removeSpell(String spellName)
     {
         spells.remove(spellName);
+        return this;
     }
 
-    public Path getSpellAudio(String spellName)
+    public String getSpellAudio(String spellName)
     {
         if (!spells.containsKey(spellName))
         {
@@ -66,5 +88,19 @@ public class CharacterModel
             );
         }
         return spells.get(spellName);
+    }
+
+    public void save(String filename) throws IOException
+    {
+        // TODO new file creation if needed?
+        new ObjectMapper().writeValue(new File(filename), this);
+    }
+
+    public static CharacterModel load(String filename) throws IOException
+    {
+        return new ObjectMapper().readValue(
+            new File(filename),
+            CharacterModel.class
+        );
     }
 }
---
+++ src/test/java/com/dearmariarenie/bardicsoundboard/models/TestCharacterModel.java
@@ -0,0 +1,30 @@
+package com.dearmariarenie.bardicsoundboard.models;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCharacterModel
+{
+    @Test
+    public void testSaveLoadCharacter() throws IOException
+    {
+        var filename = "target/test-classes/TestSaveCharacter.json";
+
+        var testOutput = new File(filename);
+        testOutput.createNewFile();
+        var expected = new CharacterModel()
+            .setName("TestChar")
+            .addSpell("Fireball", "Fireball.mp3")
+            .addSpell("Vicious Mockery", "Vicious Mockery.mp3")
+            .addSpell("Healing Word", "Healing Word.mp3");
+        expected.save(testOutput.getAbsolutePath());
+
+        var actual = CharacterModel.load(testOutput.getAbsolutePath());
+
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getSpells(), actual.getSpells());
+    }
+}
